65f7e7113172ca7556857f0853c8c24a1d74b0e6
Lærke Engelberth <laerkeengelberth@Lrkes-MacBook-Pro.local>
2020-12-22 22:10:39 +0100

    frontpage

---
+++ script.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+    document.querySelector("#menuknap").addEventListener("mousedown", toggleMenu);
+
+}
+
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("nav").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("nav").classList.contains("hidden");
+
+//    if (erSkjult == true) {
+    //        document.querySelector("#menuknap").textContent = "☰ MENU";
+    ////    } else {
+    //      //        document.querySelector("#menuknap").textContent = "✕";
+    //    }
+
+
+}
